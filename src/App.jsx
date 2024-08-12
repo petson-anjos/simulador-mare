78275912f083d94ddc81822faea000d9b6549dc8
Pirraio <g.teixeira7x@gmail.com>
2024-08-12 02:45:07 -0300

    Esfera e luzes adicionado na cena

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,13 @@ import './App.css'
 function App() {
   return (
     <Canvas>
+      <ambientLight intensity={1} color={0xf5ef38}/>
+      <directionalLight color={0xf5ef38} intensity={12} position={[1, 0, 0]}/>
+      
+      
       <mesh>
-        <sphereGeometry></sphereGeometry>
+        <sphereGeometry />
+        <meshStandardMaterial />
       </mesh>
     </Canvas>  
   )   
