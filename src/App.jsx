408b5f4b6648d1c576600e4192b6d68c3b82a166
Pirraio <g.teixeira7x@gmail.com>
2024-08-12 19:33:31 -0300

    Sliders, Box, e ícones implementados

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,88 @@
 import { Canvas, useFrame, useLoader } from '@react-three/fiber'
 import { TextureLoader } from 'three/src/loaders/TextureLoader'
-import { useRef } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { PerspectiveCamera, OrbitControls, Stars } from '@react-three/drei'
 import { Lensflare, LensflareElement } from "three/examples/jsm/objects/Lensflare.js";
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
+import IconButton from '@mui/material/IconButton';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import PauseIcon from '@mui/icons-material/Pause';
+import '@fontsource/roboto/400.css';
 import * as THREE from 'three';
 import './App.css'
+
+
+
+
+function BoxSlider() {
+  return (
+    <Box alignItems="center" 
+    sx={{ 
+      padding: 2, 
+      background: 'rgba(33, 33, 33, 0.65)',
+      color: 'white', 
+      width: 300, 
+      position: 'absolute', 
+      top: '30px', 
+      left: '10px', 
+      zIndex: 1, 
+      borderRadius: 4,
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center'}}>
+
+      <SliderValue name ="Distância da Lua" defaultValue={10}/>
+      <SliderValue name ="Massa da Lua" defaultValue={50}/>
+      <PauseButton />
+      <PlayButton />
+      
+
+    </Box>
+  )
+}
+
+function SliderValue({name, defaultValue}) {
+  return (
+    <Box sx={{ color: 'white', width: 250, position: 'relative', zIndex: 2}}>
+      <div style={{ textAlign: 'center', fontSize: 24, fontFamily: 'Roboto, "Times New Roman"'}}>{name}</div>
+      <Slider
+          size="small"
+          defaultValue={defaultValue}
+          aria-label="Small"
+          valueLabelDisplay="auto"
+          sx={{
+            color: 'white',
+            '& .MuiSlider-thumb': {
+              color: 'white',
+            },
+            '& .MuiSlider-track': {
+              color: 'white',
+            },
+            '& .MuiSlider-rail': {
+              color: 'white',
+            },
+          }}
+        />
+    </Box>
+  )
+}
+
+function PlayButton() {
+  return (
+    <IconButton aria-label="play" size="small">
+      <PlayArrowIcon fontSize="large" style={{ color: 'white' }}/>
+    </IconButton>
+  )
+}
+
+function PauseButton() {
+  return (
+    <IconButton aria-label="pause" size="small">
+      <PauseIcon fontSize="large" style={{ color: 'white'}} />
+    </IconButton>
+  )
+}
 
 let elapsedTime = 0;
 
@@ -90,6 +166,8 @@ return (
 function App() {
   
   return (
+    <div style={{ position: 'relative', width: '100%', height: '100vh' }}>
+    <BoxSlider />
     <Canvas>
       {/* Camera e Controles */}
       <PerspectiveCamera makeDefault fov={40} position={[0, 0, 10]} />
@@ -110,8 +188,8 @@ function App() {
       <Moon position={[3, 0, 0]} size={[1/3, 128, 128]}/>
       <Tide position={[0,0,0]}/>
 
-
     </Canvas>  
+    </div>
   )   
 }
 
