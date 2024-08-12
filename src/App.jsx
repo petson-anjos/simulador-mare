c4813ddfd52e510b209a9136e2ffc5370a1ee40c
Pirraio <g.teixeira7x@gmail.com>
2024-08-12 05:36:59 -0300

    Efeito Lensflare Adicionado

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { Canvas, useFrame, useLoader } from '@react-three/fiber'
 import { TextureLoader } from 'three/src/loaders/TextureLoader'
 import { useRef } from 'react'
 import { PerspectiveCamera, OrbitControls, Stars } from '@react-three/drei'
+import { Lensflare, LensflareElement } from "three/examples/jsm/objects/Lensflare.js";
 import * as THREE from 'three';
 import './App.css'
+
 
 function Earth({position, size}) {
   const earthTexture = useLoader(TextureLoader, 'earth.jpg')
@@ -51,6 +53,33 @@ function Tide({position}) {
 }
 
 
+const AddLight = ({ h, s, l, x, y, z }) => {
+    
+  const textureLoader = new THREE.TextureLoader();
+
+  const textureFlare0 = textureLoader.load( 'lensflare0_alpha.png' );
+  const textureFlare3 = textureLoader.load( 'lensflare3.png');
+  const hexangle = textureLoader.load ('hexangle.png')
+
+  const light = new THREE.PointLight( 0xffffff, 1.5, 2000, 0 );
+  light.color.setHSL( h, s, l );
+  
+  const lensflare = new Lensflare();
+
+  lensflare.addElement( new LensflareElement( textureFlare0, 1400, 0, light.color ) );
+  lensflare.addElement( new LensflareElement( textureFlare3, 60, 0.1 ) ); //0.6
+  lensflare.addElement( new LensflareElement( textureFlare3, 70, 0.15 ) ); //0.7
+  lensflare.addElement( new LensflareElement( hexangle, 120, 0.25 ) ); //0.9
+  lensflare.addElement( new LensflareElement( textureFlare3, 70, 0.35 ) ); //1
+
+  light.add( lensflare );
+
+return (
+<primitive object={light} position={[x, y, z]} />
+);
+};
+
+
 
 
 function App() {
@@ -62,19 +91,20 @@ function App() {
       <OrbitControls makeDefault minDistance={5} maxDistance={30} />
       
       {/* Luzes da cena */}
-      <ambientLight intensity={2} color={0xf5ef38}/>
-      <directionalLight color={new THREE.Color().setHSL(0.1, 0.2, 0.2)} intensity={8} position={[1, 0, 0]  }/>
+      <ambientLight intensity={2.3} color={0xf5ef38}/>
+      <directionalLight color={new THREE.Color().setHSL(0.1, 0.2, 0.2)} intensity={12} position={[1, 0, 0]  }/>
       <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade speed={3} />
 
       
       {/* Lensflare */}
-      <pointLight position={[500, 0, 0]} color={0xffffff} intensity={1.5} distance={2000}  decay={0}/>
+      <AddLight h = '0.05' s = '0.5' l = '0.9' x = '500' y = '0' z = '0' />
       
 
       {/* Terra e Lua*/}
       <Earth position={[0,0,0]} size={[1, 128, 128]}/>
       <Moon position={[3, 0, 0]} size={[1/3, 128, 128]}/>
       <Tide position={[0,0,0]}/>
+
 
     </Canvas>  
   )   
