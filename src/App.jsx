ac9993b9567d871d4ebe70343866c7ffe7627a92
Pirraio <g.teixeira7x@gmail.com>
2024-08-12 04:39:22 -0300

    Terra e Lua adicionado em cena

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,65 @@
-
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
+import { TextureLoader } from 'three/src/loaders/TextureLoader'
+import { useRef } from 'react'
+import { PerspectiveCamera, OrbitControls, Stars } from '@react-three/drei'
+import * as THREE from 'three';
 import './App.css'
 
+function Earth({position, size}) {
+  const earthTexture = useLoader(TextureLoader, 'earth.jpg')
+  const ref = useRef()
+  useFrame((state, delta) => {
+    ref.current.rotation.y += delta * 1/2
+  })
+
+  return (
+    <mesh position={position} ref={ref}>
+        <sphereGeometry args={size}/>
+        <meshStandardMaterial map={earthTexture}/>
+    </mesh>
+  )
+}
+
+function Moon({position, size}) {
+  const moonTexture = useLoader(TextureLoader, 'moon.jpg')
+  const ref = useRef()
+  useFrame((state, delta) => {
+    ref.current.rotation.y += delta * 1/2
+  })
+  
+  return (
+    <mesh position={position} ref={ref}>
+        <sphereGeometry args={size}/>
+        <meshStandardMaterial map={moonTexture}/>
+    </mesh>
+  )
+}
+
+
+
+
 function App() {
+  
   return (
     <Canvas>
-      <ambientLight intensity={1} color={0xf5ef38}/>
-      <directionalLight color={0xf5ef38} intensity={12} position={[1, 0, 0]}/>
+      {/* Camera e Controles */}
+      <PerspectiveCamera makeDefault fov={40} position={[0, 0, 10]} />
+      <OrbitControls makeDefault minDistance={5} maxDistance={30} />
       
+      {/* Luzes da cena */}
+      <ambientLight intensity={0.8} color={0xf5ef38}/>
+      <directionalLight color={new THREE.Color().setHSL(0.1, 0.2, 0.4)} intensity={12} position={[1, 0, 0]  }/>
+      <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade speed={3} />
+
       
-      <mesh>
-        <sphereGeometry />
-        <meshStandardMaterial />
-      </mesh>
+      {/* Lensflare */}
+      <pointLight position={[500, 0, 0]} color={0xffffff} intensity={1.5} distance={2000}  decay={0}/>
+      
+
+      {/* Terra e Lua*/}
+      <Earth position={[0,0,0]} size={[1, 128, 128]}/>
+      <Moon position={[3, 0, 0]} size={[1/3, 128, 128]}/>
+
     </Canvas>  
   )   
 }
